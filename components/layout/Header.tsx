bd41c3c712ba9ca3055c08cebe57d573e27d6108
AbeloByte <gobeztech20@gmail.com>
2025-11-07 22:15:22 +0300

    inital commit with a header

---
+++ components/layout/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <div className="w-[920px] h-[72px] bg-white rounded-full border-[#DEE5ED] border flex items-center justify-between  pl-[20px] pr-[14px] mt-8">
+      <div className="flex gap-2 items-center justify-center">
+        <Image src="/images/Logo.png" alt="Logo" width={38} height={38} />
+        <span className="font-semibold text-[20px]">Expense Scout</span>
+      </div>
+      <div className="font-(family-name:--font-inter) flex gap-5 items-center justify-center font-medium text-foreground-secondary ">
+        <p>Features</p>
+        <p>Testimonials</p>
+      </div>
+      <div>
+        <Button className="" variant="default" size="lg">
+          Get the App
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
