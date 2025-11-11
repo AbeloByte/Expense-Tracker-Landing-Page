ade78a579415fdd1991beb2cd28b8cf35490bf6a
AbeloByte <gobeztech20@gmail.com>
2025-11-11 22:07:03 +0300

    Enhance testimonial section with additional entries and update styling for TestimonialCard component

--- components/sections/Testimonial.tsx
+++ components/sections/Testimonial.tsx
@@ -22,7 +22,71 @@ const testimonialData = [
       "“As a student, budgeting used to be overwhelming. This app makes it so easy to track every birr I spend. It’s like having a personal finance coach in my pocket!”",
     fullname: "Lidya Tadesse",
     position: "University Student",
-    furtherClass: "ring-4 ring-primary",
+    furtherClass: "border-primary bg-primary/10",
+  },
+  {
+    image: "/User Profile/user3Profile.svg",
+    description:
+      "“We use it to track team expenses and manage monthly reports. The multi-platform access and real-time syncing save us hours every week. Highly recommended for any growing team!”",
+    fullname: "Yonas Bekele",
+    position: "Finance Manager at TechNova PLC",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user4Profile.svg",
+    description:
+      "“Finally, an app that speaks my language! Clean interface, no clutter, and I always know where my money goes. It’s made budgeting feel empowering, not stressful.”",
+    fullname: "Buzayew Girma",
+    position: "Retired Teacher",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user3Profile.svg",
+    description:
+      "“We use it to track team expenses and manage monthly reports. The multi-platform access and real-time syncing save us hours every week. Highly recommended for any growing team!”",
+    fullname: "Yonas Bekele",
+    position: "Finance Manager at TechNova PLC",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user4Profile.svg",
+    description:
+      "“Finally, an app that speaks my language! Clean interface, no clutter, and I always know where my money goes. It’s made budgeting feel empowering, not stressful.”",
+    fullname: "Buzayew Girma",
+    position: "Retired Teacher",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user3Profile.svg",
+    description:
+      "“We use it to track team expenses and manage monthly reports. The multi-platform access and real-time syncing save us hours every week. Highly recommended for any growing team!”",
+    fullname: "Yonas Bekele",
+    position: "Finance Manager at TechNova PLC",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user4Profile.svg",
+    description:
+      "“Finally, an app that speaks my language! Clean interface, no clutter, and I always know where my money goes. It’s made budgeting feel empowering, not stressful.”",
+    fullname: "Buzayew Girma",
+    position: "Retired Teacher",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user3Profile.svg",
+    description:
+      "“We use it to track team expenses and manage monthly reports. The multi-platform access and real-time syncing save us hours every week. Highly recommended for any growing team!”",
+    fullname: "Yonas Bekele",
+    position: "Finance Manager at TechNova PLC",
+    furtherClass: "",
+  },
+  {
+    image: "/User Profile/user4Profile.svg",
+    description:
+      "“Finally, an app that speaks my language! Clean interface, no clutter, and I always know where my money goes. It’s made budgeting feel empowering, not stressful.”",
+    fullname: "Buzayew Girma",
+    position: "Retired Teacher",
+    furtherClass: "",
   },
   {
     image: "/User Profile/user3Profile.svg",
@@ -65,29 +129,31 @@ const Testimonial = ({}) => {
             </div>
           </div>
         </div>
-        <div className="w-full min-w-[400px]">
-          <Swiper
-            modules={[Pagination, Navigation]}
-            spaceBetween={30}
-            slidesPerView={3.5}
-            breakpoints={{
-              768: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
-            }}
-            className="mySwiper"
-          >
-            {testimonialData.map((t, index) => (
-              <SwiperSlide key={index} className="h-full flex">
-                <TestimonialCard
-                  image={t.image}
-                  description={t.description}
-                  fullname={t.fullname}
-                  position={t.position}
-                  furtherClass={t.furtherClass}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        <div className=" ">
+          <div className=" w-screen">
+            <Swiper
+              modules={[Pagination, Navigation]}
+              spaceBetween={30}
+              slidesPerView={3.5}
+              breakpoints={{
+                768: { slidesPerView: 2 },
+                1024: { slidesPerView: 3 },
+              }}
+              className="mySwiper"
+            >
+              {testimonialData.map((t, index) => (
+                <SwiperSlide key={index} className="h-full flex w-[380px]">
+                  <TestimonialCard
+                    image={t.image}
+                    description={t.description}
+                    fullname={t.fullname}
+                    position={t.position}
+                    furtherClass={t.furtherClass}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
       </div>
     </div>
